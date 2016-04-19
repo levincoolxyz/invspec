3e2681d4604219ddcf7aeaa081ab8a851cecbc32
Feng Ling <fling@utexas.edu>
2016-04-18 22:25:31 -0500

    300

--- pushnow.sh
+++ pushnow.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if type zenity >/dev/null && [ ! `echo $DISPLAY` == "" ] && [ `$1` == "" ]
+if type zenity >/dev/null && [ ! `echo $DISPLAY` == "" ] && [ "$1" == "" ]
   then
     CommitComment=$(zenity --entry --text "Care to comment?" --entry-text "generic new commit");
-elif [ ! `$1` == "" ]; then
+elif [ ! "$1" == "" ]; then
   CommitComment="$1"
 else
   CommitComment="remote commit thru terminal; too lazy to type";
